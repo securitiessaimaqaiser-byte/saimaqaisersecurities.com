8d493d0af03ce6d0d68033f77298fb7896702d19
securitiessaimaqaiser-byte <securitiessaimaqaiser@gmail.com>
2026-01-29 17:28:15 +0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,1 +1,10 @@
+const items = document.querySelectorAll(".animate");
+
+window.addEventListener("scroll", () => {
+  items.forEach(el => {
+    if (el.getBoundingClientRect().top < window.innerHeight - 100) {
+      el.classList.add("show");
+    }
+  });
+});
 
