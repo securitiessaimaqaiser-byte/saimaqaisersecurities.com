8cc77b139ec9a0864909dcee5f41a15f82076dd0
securitiessaimaqaiser-byte <securitiessaimaqaiser@gmail.com>
2026-01-29 19:07:42 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 document.getElementById("contact-form").addEventListener("submit", e => {
   e.preventDefault();
+  if (!name.value || !email.value) {
+    alert("Please fill all fields");
+    return;
+  }
   alert("Message sent (demo)");
 });
-
