9c1b24a5c02487dfd3ec291afd1f36dae09f38b8
securitiessaimaqaiser-byte <securitiessaimaqaiser@gmail.com>
2026-01-30 11:10:17 +0500

    Update loader.js

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,17 +1,5 @@
 window.addEventListener("load", () => {
-  const loader = document.getElementById("site-loader");
-  const site = document.getElementById("site-wrapper");
-
-  site.style.display = "none";
-
   setTimeout(() => {
-    loader.style.opacity = "0";
-    loader.style.transition = "opacity .6s ease";
-
-    setTimeout(() => {
-      loader.style.display = "none";
-      site.style.display = "block";
-    }, 600);
-
-  }, 1400);
+    document.getElementById("site-loader").style.display = "none";
+  }, 2000);
 });
