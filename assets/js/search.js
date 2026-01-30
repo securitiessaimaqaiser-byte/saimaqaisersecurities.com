a2c66e9a176eaa71e671bae109f190fc1093520b
securitiessaimaqaiser-byte <securitiessaimaqaiser@gmail.com>
2026-01-30 11:10:55 +0500

    Update search.js

--- assets/js/search.js
+++ assets/js/search.js
@@ -1,6 +1,6 @@
-document.getElementById("site-search").addEventListener("input", e => {
-  const q = e.target.value.toLowerCase();
+document.getElementById("site-search").addEventListener("keyup", function () {
+  const q = this.value.toLowerCase();
   document.querySelectorAll("section").forEach(sec => {
-    sec.style.display = sec.textContent.toLowerCase().includes(q) ? "" : "none";
+    sec.style.display = sec.innerText.toLowerCase().includes(q) ? "block" : "none";
   });
 });
