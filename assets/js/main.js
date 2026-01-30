48eca8e51bf7d616b3b1eaa069aea62e1f75f099
securitiessaimaqaiser-byte <securitiessaimaqaiser@gmail.com>
2026-01-30 11:36:22 +0500

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,15 @@
-const burger = document.querySelector('.hamburger');
-const nav = document.querySelector('.nav');
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const body = document.body;
 
-burger.onclick = () => {
-  nav.classList.toggle('active');
-  burger.classList.toggle('active');
-  document.body.classList.toggle('no-scroll');
-};
+hamburger.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  body.classList.toggle("no-scroll");
+});
 
-document.querySelectorAll('.dropdown > button').forEach(btn=>{
-  btn.onclick = () => btn.parentElement.classList.toggle('open');
+document.querySelectorAll(".dropdown > button").forEach(btn => {
+  btn.addEventListener("click", () => {
+    btn.parentElement.classList.toggle("open");
+  });
 });
